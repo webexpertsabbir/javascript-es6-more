84924c68fc16b226ec5e24e09c238c4532449e59
Sabbir <webexpertsabbir@gmail.com>
2022-08-25 20:38:59 +0600

    forEach

---
+++ forEach.js
@@ -0,0 +1,11 @@
+const friends = ['Arif', 'Nasir', 'Mijan', 'Sohel'];
+friends.forEach(friend => console.log(friend))
+
+const product = [
+    {id: 1, name: 'Laptop', price: 2000},
+    {id: 2, name: 'Iphone', price: 3000},
+    {id: 3, name: 'Watch', price: 6000},
+    {id: 4, name: 'Tablet', price: 1000},
+]
+
+product.forEach(products => console.log(products));
