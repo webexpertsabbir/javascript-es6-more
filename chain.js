44af5f1c6a00923abfd4e51ca711a493f6f9c293
Sabbir <webexpertsabbir@gmail.com>
2022-08-25 18:20:05 +0600

    user

--- chain.js
+++ chain.js
@@ -2,4 +2,18 @@
 const user = [
     {id: 1, name: 'Sabbir Hossen', job: 'programmer'}
 ]
-console.log(user[0].job);
+// console.log(user[0].job);
+
+
+// get data
+
+const data = {
+    count: 5000,
+    data:[
+    {id: 1, name: 'abul', job: 'leader'},
+    {id: 2, name: 'babul', job: 'leader'}
+    ]
+}
+
+const userProftion = data.data[0].job
+// console.log(userProftion);
