66dee183b27a915e7aa3e4b9f4fe3c6a4133ce81
Sabbir <webexpertsabbir@gmail.com>
2022-08-26 11:03:42 +0600

    raltion

--- ralation.js
+++ ralation.js
@@ -18,3 +18,30 @@ console.log(camestry);
 const subject = 'physis';
 const subjectMarks = student.marks[subject];
 console.log(subjectMarks);
+
+// function min(nums) { 
+// return Math.min(nums) 
+// }
+// console.log(min( [1,3,2 ]))
+
+// const cube=x=> x*x*x; 
+// console.log(cube(2)) 
+
+// const [a, b] = [1,2,3,4,45,5]; 
+// console.log(a+b);
+
+// const {x, y, z} = {x: 1, y1: 2, z: 3};
+// console.log(y)
+
+// const nums = [1,2,3,4,5];
+// let output = nums.filter(n => n%2);
+// console.log(output);
+
+// const product = {
+//     name: 'Laptop', 
+//     model:'Yoga 3',
+//     price:49000, 
+//     dusk: '512SSD'
+//     }
+
+//     console.log(product.price);
